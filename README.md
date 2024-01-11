e05d3e4264950a9e5f41d64ba483fac9a4785c7c
Claire Y. Yurev <claire@opencortex.ai>
2024-01-11 02:32:15 -0800

    Expanded Functionality

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # EdgeStore-Expanded
 EdgeStore Client with expanded functionality: keep track of URLs, File Count, and Sizes.
 
+## Changes
+- Changed target: project has to target es2015, change applied*
+- Changed /src/app/page.tsx                                (New everything)
+- Changed /src/app/from/page.tsx                           (New everything)
+- Changed /src/app/api/edgestore/[...edgestore]/route.ts   (New everything, correct bucket, different options set)
+- Changed /src/components/multi-file-dropzone.tsx          (Added into the FileState type, plus minor initialization changes)
+
+    *Project must target es2015: "compilerOptions": "target": "es2015"...
+    Because of: "only works when using the '--downlevelIteration' flag or with a '--target' of 'es2015' or higher."
+    Things like: console.log(...urlSizeMapVFS.entries()); -OR- 
+    const merged = new Map([...urlSizeMapVFS, ...uploadedFileUrlsAndSizes]);
+    ^^^ These only work with es2015.
+
+
 # How to run
 
 1. Create an account
--- src/app/api/edgestore/[...edgestore]/route.ts
+++ src/app/api/edgestore/[...edgestore]/route.ts
@@ -1,60 +1,21 @@
-import { initEdgeStore } from "@edgestore/server";
-import {
-  CreateContextOptions,
-  createEdgeStoreNextHandler,
-} from "@edgestore/server/adapters/next/app";
-import { z } from "zod";
+import { initEdgeStore } from "@edgestore/server"
+import { createEdgeStoreNextHandler } from "@edgestore/server/adapters/next/app"
 
-type Context = {
-  userId: string;
-  userRole: "admin" | "user";
-};
-
-function createContext({ req }: CreateContextOptions): Context {
-  // get the session from your auth provider
-  // const session = getSession(req);
-  return {
-    userId: "1234",
-    userRole: "user",
-  };
-}
-
-const es = initEdgeStore.context<Context>().create();
+const es = initEdgeStore.create();
 
 const edgeStoreRouter = es.router({
-  myPublicImages: es
-    .imageBucket({
-      maxSize: 1024 * 1024 * 1, // 1MB
-    })
-    .input(
-      z.object({
-        type: z.enum(["post", "profile"]),
-      })
-    )
-    // e.g. /post/my-file.jpg
-    .path(({ input }) => [{ type: input.type }]),
-
-  myProtectedFiles: es
-    .fileBucket()
-    // e.g. /123/my-file.pdf
-    .path(({ ctx }) => [{ owner: ctx.userId }])
-    .accessControl({
-      OR: [
-        {
-          userId: { path: "owner" },
-        },
-        {
-          userRole: { eq: "admin" },
-        },
-      ],
-    }),
-});
+    requestedEstimates: es
+        .fileBucket({
+            maxSize: 16130 * 12098 * 2048, // 2048MB Server-side limit for the bucket
+            accept: ['image/*', 'video/*'], // wildcard, but this also works: ['image/jpeg', 'image/png']
+        }),
+})
 
 const handler = createEdgeStoreNextHandler({
-  router: edgeStoreRouter,
-  createContext,
-});
+    router: edgeStoreRouter,
+})
 
-export { handler as GET, handler as POST };
+// export { handler as GET, handler as POST, handler as PUT, handler as DELETE }
+export { handler as GET, handler as POST }
 
 export type EdgeStoreRouter = typeof edgeStoreRouter;
